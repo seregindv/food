9b7e43421db0964a7e98421c503915f40106c2ab
Dmitry Seregin <dmitry.seregin.7839@gmail.com>
2026-08-22 18:44:39 +0200

    Fixing pull down and info style

--- js/index.js
+++ js/index.js
@@ -239,7 +239,7 @@ async function onRefresh() {
 }
 
 function onRefreshStart(e) {
-  if (e.event.target.matches("option")) {
+  if (e.event.target.closest("dialog") || e.event.target.matches("option")) {
     e.cancel = true;
     return;
   }
